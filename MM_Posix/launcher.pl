f16842c142d5fbd588065669ef5c565ad08d0ce3
SebastianSierrajc <sebastian.sierrajc@gmail.com>
2021-10-19 19:06:07 -0500

    version de presentacion parcial 2

--- MM_Posix/launcher.pl
+++ MM_Posix/launcher.pl
@@ -4,9 +4,10 @@
 # dados parametros de entrada.
 
 $repetition = 30;
-$path = "/home/sebastian/workspace-cs/matrix-multiplication-benchmark/algorithm6/";
-@ejecutables = ("MM2f");
-@matrixSize = ("100", "200", "400", "800", "1000", "1400", "1800", "2000", "2400", "4000"); 
+$path = "/home/sebastian/workspace-cs/matrix-multiplication-benchmark/MM_Posix/";
+@ejecutables = ("MM1p");
+# @matrixSize = ("100", "200", "400", "800", "1000", "1200", "2000", "4000");
+@matrixSize = ("500", "1000", "1200", "2000");
 @threads = ("1", "2", "4", "8");
 
 foreach $exe (@ejecutables){
