ea34ca5f9783018b069f83f5e7d2c8032069ec3a
SebastianSierrajc <sebastian.sierrajc@gmail.com>
2021-10-22 20:21:21 -0500

    mejora en el script para apsar los datos a excel

--- MM_OpenMP/toexcel.py
+++ MM_OpenMP/toexcel.py
@@ -4,43 +4,53 @@ import numpy as np
 import matplotlib.pyplot as plt
 from openpyxl import load_workbook, Workbook
 
-def readData(data, path, program, sizes, thread):
-    for i, size in enumerate(sizes):
-        f = open(f"{path}{program}-size{size}-T{thread}.txt")
-        for j, l in enumerate(f):
-            data[j][i] = float(l)
+path = '/home/sebastian/workspace-cs/matrix-multiplication-benchmark/'
+algorithm = "MM_OpenMP/"
+program = 'MM1c'
+N = 30
+sizes = [ "500", "1000", "1200", "2000"] 
+threads = ['1', '2', '4', '8']
+
+def readData(data, thread, start):
+    for i, size in enumerate(sizes, start=1):
+        if i != 0:
+            f = open(f"{path}{algorithm}outputs/{program}-size{size}-T{thread}.txt")
+            for j, l in enumerate(f, start=N*start):
+                data[j][i] = float(l)
+
 
 def main():
-    path = '/home/sebastian/workspace-cs/matrix-multiplication-benchmark/'
-    algorithm = "MM_OpenMP/"
-    program = 'MM1c'
-    # sizes = [ "800", "1000", "1400", "1800", "2000", "2400", "4000"] 
-    sizes = [ "500", "1000", "1200", "2000"] 
-    threads = ['1', '2', '4', '8']
-    data = np.zeros([30, 4])
-    try:
+    data = np.zeros([N * len(sizes), len(threads)+1])
+
+    if os.path.exists(path+"parcial2.xlsx"):
         book = load_workbook(path+"parcial2.xlsx")
-    except FileNotFoundError:
-       book = Workbook()
-    except:
-        if os.path.exists("../parcial2.xlsx"):
-            os.remove(path+"parcial2.xlsx")
-     
-   
+    else:
+        book = Workbook()
+      
     try:
         writer = pd.ExcelWriter(path+"parcial2.xlsx", engine = 'openpyxl')
         writer.book = book
 
-        for thread in threads:
-            readData(data, path+algorithm+"outputs/", program, sizes, thread)
-            data /= 1000000
-            df = pd.DataFrame(data=data, columns=sizes)
-            df.to_excel(writer, sheet_name=program+"-T"+thread)
-    
+        for i, thread in enumerate(threads):
+            readData(data, thread, i)
+            data[i*N:, 0] = thread
+        
+        data /= 1000000
+
+        for i, thread in enumerate(threads):
+            data[i*N:i*N+30, 0] = thread
+        
+        df = pd.DataFrame(data=data, columns=["Threads"]+sizes)
+        means = df.groupby("Threads").mean()
+
+        df.to_excel(writer, sheet_name=program+"-values")
+        means.to_excel(writer, sheet_name=program+"results")
+
         writer.save()
         writer.close()
-    except:
-        os.remove(path+"parcial2.xlsx")
+
+    except Exception as e:
+        print(e)
 
 
 if __name__ == '__main__':
